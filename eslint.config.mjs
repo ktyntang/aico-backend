1ce772c4c5c9cec17bc5daa79663d19bee471c7c
ktyntang <ktyntang@hotmail.com>
2026-04-18 13:52:42 +0100

    refactor: remove deprecated function

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,14 @@
+import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
 
-export default tseslint.config(
+export default defineConfig([
   ...tseslint.configs.recommended,
   prettierConfig,
   {
     rules: {
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     },
-  },
-  {
     ignores: ['dist/**', 'node_modules/**', 'data/**'],
   },
-);
+]);
